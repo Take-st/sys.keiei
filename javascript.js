4ef41b7fa02aa10c597ffd3d6f011ac1d591a48a
Take-st <s.takewuw@gmail.com>
2018-09-27 19:25:20 +0900

    finish except responsive

--- javascript.js
+++ javascript.js
@@ -2,12 +2,34 @@ $(function () {
   // scrollイベントを取得した際の処理を定義
   $(window).on("scroll", function () {
     // scrollTop()が0より大きい場合
-    if ($(this).scrollTop() > $('.message-wrapper').scrollTop()) {
+    if ($(this).scrollTop() > $('.message-wrapper').offset().top) {
       // ヘッダーバーをslideDownして表示
       $("header").slideDown();
     } else {
       // ヘッダーバーをslideUpして非表示
       $("header").slideUp();
+    }
+  });
+});
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
+
+$(function () {
+  $('input[name="setting"]:radio').change(function () {
+    var radioval = $(this).val();
+    if (radioval == "myself") {
+      $('.manual').show();
+    } else if (radioval == "auto") {
+      $('.manual').hide();
     }
   });
 });
